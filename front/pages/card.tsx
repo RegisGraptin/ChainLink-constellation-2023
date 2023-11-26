2fcb68a496474630331718f70fa80d2dc15f8697
rere <regis-graptin@sfr.fr>
2023-11-26 20:02:57 +0100

    feat: add tailwindcss

--- front/pages/card.tsx
+++ front/pages/card.tsx
@@ -5,27 +5,9 @@ import styles from '../styles/Home.module.css';
 
 const Card: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Card Page</title>
-      </Head>
-
-      <main className={styles.main}>
-        <ConnectButton />
-
-        <h1 className={styles.title}>
-          Welcome to <a href="">RainbowKit</a> + <a href="">wagmi</a> +{' '}
-          <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-      </main>
-
-      <footer className={styles.footer}>
-        <a href="https://rainbow.me" rel="noopener noreferrer" target="_blank">
-          Made with ❤️ by your frens at 🌈
-        </a>
-      </footer>
-    </div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   );
 };
 
--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -2,6 +2,8 @@ import { ConnectButton } from '@rainbow-me/rainbowkit';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import { Header } from '../components/Header';
+
 
 const Home: NextPage = () => {
   return (
@@ -16,6 +18,9 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
+
+        <Header />
+
         <ConnectButton />
 
         <h1 className={styles.title}>
