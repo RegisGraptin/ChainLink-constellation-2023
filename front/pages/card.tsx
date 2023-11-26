5d5d9bbb213d0523c4321ee6f2d2c5d5117edc6c
rere <regis-graptin@sfr.fr>
2023-11-26 19:30:10 +0100

    feat: create frontend project with wagmi wallet connect

---
+++ front/pages/card.tsx
@@ -0,0 +1,32 @@
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+const Card: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Card Page</title>
+      </Head>
+
+      <main className={styles.main}>
+        <ConnectButton />
+
+        <h1 className={styles.title}>
+          Welcome to <a href="">RainbowKit</a> + <a href="">wagmi</a> +{' '}
+          <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+
+      </main>
+
+      <footer className={styles.footer}>
+        <a href="https://rainbow.me" rel="noopener noreferrer" target="_blank">
+          Made with ❤️ by your frens at 🌈
+        </a>
+      </footer>
+    </div>
+  );
+};
+
+export default Card;
