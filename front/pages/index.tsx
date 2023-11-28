7e751bfe9d4ade0c1ae3d373dd512df57850c9a3
rere <regis-graptin@sfr.fr>
2023-11-28 22:09:40 +0100

    feat: create landing page simple desing

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -1,9 +1,6 @@
-import { ConnectButton } from '@rainbow-me/rainbowkit';
 import type { NextPage } from 'next';
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
 import { Header } from '../components/Header';
-import { useContractWrite, usePrepareContractWrite } from 'wagmi'
+import { useContractWrite } from 'wagmi'
 import wagmigotchiABI from '../../contract/artifacts/contracts/ERC6551Registry.sol/ERC6551Registry.json'
 import {useState, useEffect} from 'react'
 import { useAccount } from 'wagmi';
@@ -31,8 +28,26 @@ const Home: NextPage = () => {
     <main>
       <Header />
 
-      <button onClick={() => write()} className='mt-10 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow'>Create Gift Card</button>
+      <section className="container mx-auto flex items-center justify-center min-h-screen">
 
+      <div className="w-2/3 p-8">
+        <h1 className="text-4xl font-bold mb-4">Wrapped Gift</h1>
+        <p className="text-lg text-gray-700">
+          Create a gift for your loved one.<br /> 
+          Mint a NFT now to pool the fund.<br /> 
+          Then, send it to your loved one on his birthday.
+        </p>
+        <button onClick={() => write()} className='mt-10 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow'>Create Gift Card</button>
+      </div>
+
+
+      
+
+      <div className="w-1/3">
+        <img className="w-full h-auto max-w-lg rounded-2xl" src="/home.jpg" alt="gift image" />
+      </div>
+
+      </section>
     </main>
   );
 };
