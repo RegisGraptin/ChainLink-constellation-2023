13d243e285a35e2e778fdc081d476ebb272016a9
Riki0923 <83667276+Riki0923@users.noreply.github.com>
2023-12-01 10:51:33 +0100

    Krisztian - hardhat scripts for CCIP

---
+++ contract/scripts/ccipAllowChain.js
@@ -0,0 +1,31 @@
+require("hardhat");
+require("dotenv").config();
+
+
+async function main() {
+  const TokenTransferor = await ethers.getContractFactory("TokenTransferor");
+  const tokentransferor = await TokenTransferor.attach(process.env.NEXT_PUBLIC_CCIP_CONTRACT_ADDRESS_AVALANCHE);
+
+  // function arguments for sending ( this is for avalanche )
+  const destinationChain = "16015286601757825753"; // Sepolia
+  const receiver = process.env.NEXT_PUBLIC_TBD_ACCOUNT;
+  const tokenAddress = "0xD21341536c5cF5EB1bcb58f6723cE26e8D8E90e4"; // CCIP - BnM
+  const amount = 1000000000000000;
+
+  const tx = await tokentransferor.allowlistDestinationChain(destinationChain, true);
+  const receipt = await tx.wait();
+//   const address = await registry.account(implementation, chainID, tokenAddress, tokenId, salt)
+  
+  if(receipt.status){
+   console.log(`Sepolia destination has been allowed`);
+  }
+   else{
+    console.log("allow Destination process has failed");
+  }
+
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
--- contract/scripts/ccipTransfer.js
+++ contract/scripts/ccipTransfer.js
@@ -7,7 +7,7 @@ async function main() {
   const tokentransferor = await TokenTransferor.attach(process.env.NEXT_PUBLIC_CCIP_CONTRACT_ADDRESS_AVALANCHE);
 
   // function arguments for sending ( this is for avalanche )
-  const destinationChain = 16015286601757825753; // Sepolia
+  const destinationChain = "16015286601757825753"; // Sepolia
   const receiver = process.env.NEXT_PUBLIC_TBD_ACCOUNT;
   const tokenAddress = "0xD21341536c5cF5EB1bcb58f6723cE26e8D8E90e4"; // CCIP - BnM
   const amount = 1000000000000000;
@@ -15,9 +15,11 @@ async function main() {
   const tx = await tokentransferor.transferTokensPayNative(destinationChain, receiver, tokenAddress, amount);
   const receipt = await tx.wait();
 //   const address = await registry.account(implementation, chainID, tokenAddress, tokenId, salt)
+
+  console.log("transaction hash is:", receipt.hash)
   
   if(receipt.status){
-   console.log(`CCIP transaction succesffull, ${amount} CCIP-BnM has been sent to ${receiver}`);
+   console.log(`CCIP transaction succesfull, ${amount} CCIP-BnM has been sent to ${receiver}`);
   }
    else{
     console.log("CCIP transaction failed");
