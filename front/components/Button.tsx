b1933a57a5b542c67e4891878bb156f42da2c4ef
rere <regis-graptin@sfr.fr>
2023-12-09 15:54:46 +0100

    feat: add date picker

--- front/components/Button.tsx
+++ front/components/Button.tsx
@@ -1,20 +1,21 @@
 import React from 'react'
 import { useState } from 'react'
 
-export const Button = ({initialValue, children}:any) => {
+export const Button = ({initialValue, children, onClick}: any) => {
     const [value, setValue] = useState(initialValue)
     const handleValueChange = (e:any) =>{
         setValue(e.target.value)
     }
 
-    const handleButtonClick = () => {
-        console.log("Button clicked with value", value)
-        setValue("")
-    }
+    const handleClick = () => {
+      if (onClick) {
+        onClick();
+      }
+    };
 
   return (
     <div className='flex flex-col'>
-        <button onClick={handleButtonClick} className="text-red-400 hover:text-white bg-white hover:bg-red-400 font-bold border-2 border-red-400 rounded-3xl w-52 h-12 transition-all duration-200">{children}</button>
+        <button onClick={handleClick} className="text-red-400 hover:text-white bg-white hover:bg-red-400 font-bold border-2 border-red-400 rounded-3xl w-52 h-12 transition-all duration-200">{children}</button>
     </div>
   )
 }
--- front/components/CreateGift/Step3.tsx
+++ front/components/CreateGift/Step3.tsx
@@ -1,14 +1,17 @@
 import { Button } from "../Button"
 import { Inputfield } from "../Inputfield"
 
-export const Step3 = (props:any) => {
+export const Step3 = (props: any) => {
 
     return (
         <>
             <div className="flex flex-col justify-center items-center space-y-16 mt-20">
-            <p className="text-red-400 font-bold text-2xl">Choose ENS Name for Wrap</p>
-            <Inputfield/>
-            <Button onClick={() => props.nextPage}>Set ENS Name</Button>
+                <p className="text-red-400 font-bold text-2xl">Choose ENS Name for Wrap</p>
+                <Inputfield />
+                <div className="columns-2">
+                    <Button onClick={() => props.nextPage()}>Set ENS Name</Button>
+                    <Button onClick={() => props.nextPage()}>Skip</Button>
+                </div>
             </div>
         </>
     )
