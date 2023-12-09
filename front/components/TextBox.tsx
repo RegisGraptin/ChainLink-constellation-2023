7520cbd3e70e3b9a4741b40ffbd255fde59f12db
Riki0923 <83667276+Riki0923@users.noreply.github.com>
2023-12-09 09:53:18 +0100

    Krisztian - textbox component

--- front/components/TextBox.tsx
+++ front/components/TextBox.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { useState } from 'react'
+
 
 const TextBox = () => {
+    const[value, setValue] = useState("")
+    const handleChange = (e:any) => setValue(e.target.value)
+
   return (
-    <div className='h-32 w-32 bg-white border-2 border-red-400 text-red-400 text-center items-center flex justify-center'></div>
+    <div className='h-32 w-32 bg-white border-2 border-red-400 text-red-400 text-center items-center flex justify-center'>
+        <input type='text' value={value} onChange={handleChange}/>
+        <p>{value}</p>
+    </div>
   )
 }
 
