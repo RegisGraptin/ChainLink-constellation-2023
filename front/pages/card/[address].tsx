1bd0542a42f86334c7f675b4cf07c03239f0b116
rere <regis-graptin@sfr.fr>
2023-12-10 15:40:01 +0100

    add chainlink function

--- front/pages/card/[address].tsx
+++ front/pages/card/[address].tsx
@@ -276,12 +276,13 @@ const Card: NextPage = () => {
                   Send funds
                 </button>
 
-                {/* <button
+                
+                <button
                   type="button"
-                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-                  onClick={() => createSubscriptionChainLinkFunction()}>
+                  className="text-red-400 hover:text-white bg-white hover:bg-red-400 font-bold border-2 border-red-400 rounded-3xl w-64 h-12 transition-all duration-200"
+                  onClick={() => generateGPTImage()}>
                   Generate an image using chainlink function & chat gpt
-                </button> */}
+                </button>
               </section>
             </div>
           )
